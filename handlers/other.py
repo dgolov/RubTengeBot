829d32a60eb461da862a110a3b652b871d7e3398
dgolov <dmitriy.evgenyevich@gmail.com>
2022-10-10 03:23:21 +0600

    Added taxi phone

--- handlers/other.py
+++ handlers/other.py
@@ -1,4 +1,5 @@
 from aiogram import types, Dispatcher
+from create_bot import dp
 from logging_settings import logger
 from patterns import mach_answer
 
@@ -8,6 +9,13 @@ async def echo(message: types.Message):
     logger.info(f'[other - echo] {message.from_user.username} - message: {message.text}')
     answer = await mach_answer(message)
     await message.answer(answer)
+
+
+@dp.message_handler(lambda message: 'такси' in message.text)
+async def taxi(message: types.Message):
+    """ Get taxi phone """
+    logger.info(f'[other - taxi] {message.from_user.username} - message: {message.text}')
+    await message.answer('Такси Дастар (Капчагай): +77277240500')
 
 
 def register_handlers_others(dispatcher: Dispatcher):
