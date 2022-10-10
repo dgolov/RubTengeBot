296a20049acb05adfd12c9cab46522202d302315
dgolov <dmitriy.evgenyevich@gmail.com>
2022-10-11 02:42:50 +0600

    added check_category

--- engine.py
+++ engine.py
@@ -40,10 +40,25 @@ class EngineSessionFactory:
             ret = session.query(Category).filter_by(slug=slug).first()
         return ret
 
+    def check_category(self, category):
+        """ Вычисление актуальной категории из текста сообщения """
+        categories = self.get_all_categories()
+        other = None
+
+        for item_query in categories:
+            if category.lower() in item_query.name.lower():
+                return item_query
+            if 'other' in item_query.slug:
+                other = item_query
+        return other
+
     def set_cost(self, user_id, data):
         """ Занести расход в базу данных """
+        category_data = data.get('category')
         user = self.get_user_by_telegram_id(user_id)
-        category = self.get_category_by_slug(data.get('category'))
+        category = self.get_category_by_slug(category_data) if category_data.startswith('category') \
+            else self.check_category(category_data)
+
         cost = Cost(
             user_id=user.id,
             category_id=category.id,
